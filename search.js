986d1d956a9425c319738a93b3f20a39aa746648
Matthew <mngz636@gmail.com>
2025-02-10 10:05:26 -0800

    Create search.js

---
+++ search.js
@@ -0,0 +1,26 @@
+
+var search_index = 0;
+  
+  function search(txt){
+
+   var games =  e('contents').getElementsByTagName('p');
+
+	  if(search_index<menu.length){
+		 if(txt.length>4){
+	  if(menu[search_index].toLowerCase().includes(txt.toLowerCase())){
+
+		  e('menu').innerHTML = '';
+		   menu_index = search_index;
+		  serve_menu(menu[search_index]);
+		 
+		  
+	  }else{
+		  search(txt);
+		  search_index+=1;
+	  }
+	  
+		 } 
+  }else{
+	          search_index=0;
+  }
+  }
