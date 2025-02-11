0a57919c80e9eb797d4a9628e4c6ea804ba62b8b
Matthew <mngz636@gmail.com>
2025-02-11 04:10:57 -0800

    Update search.js

--- search.js
+++ search.js
@@ -5,13 +5,16 @@ var search_index = 0;
 
 	  if(search_index<menu.length){
 		 if(game.length>4){
-	  if(menu[search_index].toLowerCase().includes(game.toLowerCase())){
+
+		var menu_item = menu[search_index].toLowerCase();	
+		var search_item = game.toLowerCase();
+			 
+	  if(menu_item.includes(search_item)){
 
 		  e('menu').innerHTML = '';
 		   menu_index = search_index;
 		  serve_menu(menu[search_index]);
-		 
-		  
+	
 	  }else{
 		  search(game);
 		  search_index+=1;
